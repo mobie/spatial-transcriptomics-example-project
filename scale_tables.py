d96426dd74a9485512b554396900dab73c059500
Constantin Pape <c.pape@gmx.net>
2022-09-27 12:53:30 +0200

    Scale tables

---
+++ scale_tables.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def scale_table(table_path):
+    scale = {"z": 4.0, "y": 0.11, "x": 0.11}
+    tab = pd.read_csv(table_path, sep="\t")
+    for name, sc in scale.items():
+        tab[name] = tab[name] * sc
+    tab.to_csv(table_path, sep="\t", float_format="%.4f")
+
+
+def main():
+    scale_table("./data/pos42-spatial-transcriptomics/tables/transcriptome/default.tsv")
+    scale_table("./data/pos42-spatial-transcriptomics/tables/transcriptome-small/default.tsv")
+
+
+if __name__ == "__main__":
+    main()

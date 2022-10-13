56652298f96fde9fdd9f1318e6b5350241d1c75b
Constantin Pape <c.pape@gmx.net>
2022-10-13 21:57:29 +0200

    Add another spot source

--- add_spot_source.py
+++ add_spot_source.py
@@ -3,20 +3,22 @@ import mobie
 import pandas as pd
 
 
-def add_spot_source(name, point_fraction=None):
-    ds_folder = "./data/pos42-spatial-transcriptomics"
+def add_spot_source(name, point_fraction=None, shuffle=False):
+    ds_folder = "./data/pos42"
 
     table_folder = os.path.join(ds_folder, "tables", name)
     os.makedirs(table_folder, exist_ok=True)
     table_path = os.path.join(table_folder, "default.tsv")
 
     # input_table = "/home/pape/Downloads/segmentedData-Tim-102219-Pos42-1error-sqrt6-2020-05-22.csv"
-    input_table = "./data/pos42-spatial-transcriptomics/tables/transcriptome/default.tsv"
+    input_table = "./data/pos42/tables/transcriptome/default.tsv"
     table = pd.read_csv(input_table, sep="\t")
     table["z"] = table["z"].astype("float64")
     if point_fraction is not None:
         print(table.shape)
         n_rows = int(point_fraction * len(table))
+        if shuffle:
+            table = table.sample(frac=1)
         table = table[:n_rows]
         print(table.shape)
 
@@ -26,13 +28,18 @@ def add_spot_source(name, point_fraction=None):
 
     metadata = mobie.metadata.read_dataset_metadata(ds_folder)
     metadata["sources"][name] = {
-        "spots": {"tableData": {"tsv": {"relativePath": f"tables/{name}"}}},
+        "spots": {
+            "boundingBoxMax": [225.28, 225.28, 24.0],
+            "boundingBoxMin": [0.0, 0.0, 0.0],
+            "tableData": {"tsv": {"relativePath": f"tables/{name}"}},
+            "unit": "micrometer",
+        },
     }
     mobie.metadata.write_dataset_metadata(ds_folder, metadata)
 
 
 def add_spot_display(name):
-    ds_folder = "./data/pos42-spatial-transcriptomics"
+    ds_folder = "./data/pos42"
 
     spot_view = {
         "uiSelectionGroup": "spots",
@@ -40,8 +47,11 @@ def add_spot_display(name):
         "sourceDisplays": [
             {
                 "spotDisplay": {
+                    "lut": "glasbey",
                     "name": name,
+                    "opacity": 1.0,
                     "sources": [name],
+                    "spotRadius": 0.25,
                 }
             }
         ]
@@ -52,15 +62,17 @@ def add_spot_display(name):
     mobie.metadata.write_dataset_metadata(ds_folder, metadata)
 
 
-def add_spots(small):
+def add_spots(small, shuffle=False):
     name = "transcriptome-small" if small else "transcriptome"
+    if shuffle:
+        name += "-alt"
     point_fraction = 0.1 if small else None
-    add_spot_source(name, point_fraction=point_fraction)
+    add_spot_source(name, point_fraction=point_fraction, shuffle=shuffle)
     add_spot_display(name)
 
 
 def add_combined_view():
-    ds_folder = "./data/pos42-spatial-transcriptomics"
+    ds_folder = "./data/pos42"
     metadata = mobie.metadata.read_dataset_metadata(ds_folder)
 
     combined_view = metadata["views"]["default"]
@@ -79,9 +91,10 @@ def add_combined_view():
 
 
 def main():
-    add_spots(False)
-    add_spots(True)
-    add_combined_view()
+    add_spots(True, True)
+    # add_spots(False)
+    # add_spots(True)
+    # add_combined_view()
 
 
 if __name__ == "__main__":
